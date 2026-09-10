6bb8ffd82edb76c18207d6cdc4c565bf3443dcc9
Jefferson Lima <jefferson.lima@locaweb.com.br>
2026-09-10 08:55:27 -0300

    add helm and kind

--- 01 - Preparação do Ambiente/kind-setup/deploy-nginx-ingress.sh
+++ 01 - Preparação do Ambiente/kind-setup/deploy-nginx-ingress.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
 
+# Instala Kind
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.33.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.33.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Instala Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
 echo "Instalando ingress-nginx no Kind (sem webhook admission)..."
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
 
 helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
